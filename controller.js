16f30aabe80e6a660b7cb001254688a6bc22eb11
Billy Murray <billy.murray@oliveai.com>
2020-07-23 12:33:07 -0400

    Updated to use new storage methods

--- controller.js
+++ controller.js
@@ -1,8 +1,5 @@
 class Controller {
   constructor() {
-    this.config = {
-      foo: 'bar',
-    };
     this.style = {
       backgroundColor: '#fff',
       highlightColor: '#651fff',
@@ -10,34 +7,42 @@ class Controller {
     };
   }
 
-  start(host) {
+  async start(host) {
     this.host = host;
+
+    // setup defaults
+    if (!(await this.host.storageHasKey("counter"))) {
+      await this.host.storageWrite("counter", JSON.stringify(0));
+    }
   }
 
   stop() {
+    this.host = null;
   }
 
-  config() {
-    return this.config;
-  }
+  async onEvent(event) {
+    // ignore any non-text events
+    if (!event.data.text) {
+      return
+    }
 
-  setConfig(config) {
-    this.config = config;
-  }
+    let counter = JSON.parse(await this.host.storageRead("counter"));
+    
+    // increment counter
+    counter += 1;
+    await this.host.storageWrite("counter", JSON.stringify(counter));
 
-  onEvent(event) {
-    if (event.data.text) {
-      this.host.emitWhisper({
-        icon: 'bathtub',
-        label: 'Example Controller Node',
-        markdown: `# New Text Event
+    this.host.emitWhisper({
+      icon: 'bathtub',
+      label: 'Example Controller Node',
+      markdown: `# New Text Event
 \`\`\`
 ${event.data.text.trim()}
 \`\`\`
+Counter: ${counter}
 `,
-        style: this.style,
-      });
-    }
+      style: this.style,
+    });
   }
 }
 
